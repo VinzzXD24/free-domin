75eb656f703f08d3d4a5cea2da88bbebdd67c967
VinzzXD24 <167677323+VinzzXD24@users.noreply.github.com>
2025-02-15 21:57:35 +0700

    Delete api directory

--- api/domains.js
+++
@@ -1,9 +0,0 @@
-const domains = require('../domains');
-
-module.exports = (req, res) => {
-  if (req.method === 'GET') {
-    res.status(200).json(Object.keys(domains));
-  } else {
-    res.status(405).end(); // Method Not Allowed
-  }
-};
