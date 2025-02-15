a9636b286e8c2f2d736574ea14431c9411389e96
VinzzXD24 <167677323+VinzzXD24@users.noreply.github.com>
2025-02-15 15:09:07 +0700

    Update domains.js

--- api/domains.js
+++ api/domains.js
@@ -1,6 +1,7 @@
 const domains = require('../domains');
 
 module.exports = (req, res) => {
+  console.log('Request method:', req.method);
   if (req.method === 'GET') {
     res.status(200).json(Object.keys(domains));
   } else {
