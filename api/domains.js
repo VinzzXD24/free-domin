d35c374e5c99b2176f80b524334dfeb91aeeb5f8
VinzzXD24 <167677323+VinzzXD24@users.noreply.github.com>
2025-02-15 15:13:28 +0700

    Update domains.js

--- api/domains.js
+++ api/domains.js
@@ -1,10 +1,9 @@
 const domains = require('../domains');
 
 module.exports = (req, res) => {
-  console.log('Request method:', req.method);
   if (req.method === 'GET') {
     res.status(200).json(Object.keys(domains));
   } else {
-    res.status(405).end();
+    res.status(405).end(); // Method Not Allowed
   }
 };
