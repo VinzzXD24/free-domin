b2a7eab91ac4fdd6a321cc56f71fd3a73bbafb33
VinzzXD24 <167677323+VinzzXD24@users.noreply.github.com>
2025-02-15 14:43:08 +0700

    Create domain.js

---
+++ domain.js
@@ -0,0 +1,32 @@
+const domains = {
+  "serverku.biz.id": {
+    zone: "4e4feaba70b41ed78295d2dcc090dd3a",
+    apitoken: process.env.CF_TOKEN_1
+  },
+  "privatserver.my.id": {
+    zone: "699bb9eb65046a886399c91daacb1968",
+    apitoken: process.env.CF_TOKEN_2
+  },
+  "panelwebsite.biz.id": {
+    zone: "2d6aab40136299392d66eed44a7b1122",
+    apitoken: process.env.CF_TOKEN_3
+  },
+  "mypanelstore.web.id": {
+    zone: "c61c442d70392500611499c5af816532",
+    apitoken: process.env.CF_TOKEN_4
+  },
+  "pteroserver.us.kg": {
+    zone: "f693559a94aebc553a68c27a3ffe3b55",
+    apitoken: process.env.CF_TOKEN_5
+  },
+  "digitalserver.us.kg": {
+    zone: "df13e6e4faa4de9edaeb8e1f05cf1a36",
+    apitoken: process.env.CF_TOKEN_6
+  },
+  "shopserver.us.kg": {
+    zone: "54ca38e266bfdf2dcdb7f51fd79c2db5",
+    apitoken: process.env.CF_TOKEN_7
+  }
+};
+
+module.exports = domains;
